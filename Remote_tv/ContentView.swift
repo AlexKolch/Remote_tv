6486442baa4bb92dbb00f3728a88ddd0baa39ba9
Alex Kolch <alexkolch.ios@gmail.com>
2025-05-25 12:28:06 +0300

    Initial Commit

---
+++ Remote_tv/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Remote_tv
+//
+//  Created by Алексей Колыченков on 25.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Remote_tv/Remote_tvApp.swift
@@ -0,0 +1,17 @@
+//
+//  Remote_tvApp.swift
+//  Remote_tv
+//
+//  Created by Алексей Колыченков on 25.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Remote_tvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
